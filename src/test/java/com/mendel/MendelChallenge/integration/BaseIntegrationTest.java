680fb97ae63a3ab5b915307e3d95bb7f775319b8
TomasReisenman <tom.manrey@gmail.com>
2026-01-23 14:17:40 -0300

    Improving test execution

--- src/test/java/com/mendel/MendelChallenge/integration/BaseIntegrationTest.java
+++ src/test/java/com/mendel/MendelChallenge/integration/BaseIntegrationTest.java
@@ -1,27 +1,28 @@
 package com.mendel.MendelChallenge.integration;
 
 import io.restassured.RestAssured;
-import io.restassured.config.LogConfig;
-import io.restassured.config.RestAssuredConfig;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseIntegrationTest {
+
+    @LocalServerPort
+    protected int port;
 
     @BeforeAll
     static void setupLogging() {
         enableLoggingOfRequestAndResponseIfValidationFails();
 
-        RestAssured.config = RestAssuredConfig.config()
-                .logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails());
     }
 
     @BeforeEach
     void setupRestAssured() {
-        RestAssured.port = 8080;
+        RestAssured.port = port;
         RestAssured.baseURI = "http://localhost";
-
     }
 }
